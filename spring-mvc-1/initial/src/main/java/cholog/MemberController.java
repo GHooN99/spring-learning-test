1f009d36e1cd7015325914a2a2c03c100cf79caa
ghoon99 <ghoon99@kakao.com>
2024-09-11 17:35:17 +0900

    feat:   ?name= 요청이 들어왔을 때 해당 값을 hello.html에서 사용할 수 있도록 추가

--- spring-mvc-1/initial/src/main/java/cholog/MemberController.java
+++ spring-mvc-1/initial/src/main/java/cholog/MemberController.java
@@ -1,16 +1,26 @@
 package cholog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class MemberController {
 
     @GetMapping("/hello")
-    public String world() {
+    public String world(@RequestParam(value = "name", required = false) Optional<String> name, Model model) {
         // TODO: /hello 요청 시 resources/templates/static.html 페이지가 응답할 수 있도록 설정하세요.
+        if (name.isEmpty()) {
+            return "static";
+        }
+
         // TODO: 쿼리 파라미터로 name 요청이 들어왔을 때 해당 값을 hello.html에서 사용할 수 있도록 하세요.
-        return "static";
+        model.addAttribute("name", name.orElseGet(() -> "world"));
+        return "hello";
+
     }
 
     public Person json() {
