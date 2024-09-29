71bded82fd3967a880e83b166a40b86de301868b
ghoon99 <ghoon99@kakao.com>
2024-09-29 14:48:51 +0900

    feat: customers row의 개수 반환 메서드 추가

--- spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
@@ -31,7 +31,7 @@ public class QueryingDAO {
      */
     public int count() {
         //TODO : customers 디비에 포함되어있는 row가 몇개인지 확인하는 기능 구현
-        return 0;
+        return this.jdbcTemplate.queryForObject("SELECT COUNT(*) FROM CUSTOMERS", Integer.class);
     }
 
     /**
